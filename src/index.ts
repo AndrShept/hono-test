094e5f63b22aea8941ec1330a1b85175b33a0913
lolokos86 <lolokos1986@gmail.com>
2025-01-23 00:37:38 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import type { Server as HTTPServer } from 'node:http';
+
+export const app = new Hono().basePath('/api');
+dotenv.config();
+app.use(logger());
+app.use(cors());
+
+app.get('/hi', (c) => {
+  console.log(process.env.MY_VARIABLE);
+  console.log(process.env.ref);
+  return c.text('Hello Hono!');
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
+
+const httpServer = serve({
+  fetch: app.fetch,
+  port: port,
+});
+
+const io = new Server(httpServer as HTTPServer, {
+  /* options */
+});
+
+io.on('connection', (socket) => {
+  console.log('user connect' + socket.id);
+  // ...
+
+  socket.on('disconnect', () => {
+    console.log('disconnect' + socket.id);
+  });
+});
